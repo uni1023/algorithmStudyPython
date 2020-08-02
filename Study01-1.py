24b3ec3eb43398f63113d64b59d0c179a9b08ec0
uni1023 <yoonsy1023@gmail.com>
2020-08-02 18:17:16 +0900

    모두의 파이썬 알고리즘 예제 01-02까지 진행함

---
+++ Study01-1.py
@@ -0,0 +1,13 @@
+# 1부터 n까지 연속한 숫자의 합을 구하는 알고리즘 1
+# 입력: n
+# 출력: 1부터 n까지의 숫자를 더한 값
+
+def sum_n(n):
+    s = 0  # 합을 계산할 변수
+    for i in range(1, n + 1):  # 1부터 n까지 반복(n+1은 제외)
+        s = s + i
+    return s
+
+
+print(sum_n(10))
+print(sum_n(100))
